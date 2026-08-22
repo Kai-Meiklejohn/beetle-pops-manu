c50cd632726b692418b27b5e3e7245abd1f435e2
Kai Meiklejohn <kaim.meiklejohn@gmail.com>
2026-08-23 09:56:35 +1200

    make horizontal movement frame-rate independent

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,22 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "Beetle Pops a Manu!" );
-	sf::CircleShape shape( 100.f );
+	sf::RenderWindow window(
+		sf::VideoMode({ 800, 600 }),
+		"Beetle Pops a Manu!"
+	);
+
+	sf::CircleShape shape{ 25.f };
 	shape.setFillColor( sf::Color::Green );
+
+	sf::Clock clock;
+	constexpr float movementSpeed{ 200.f };
+
 
 	while ( window.isOpen() )
 	{
+		const float deltaTime{ clock.restart().asSeconds() };
+
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
@@ -16,11 +26,11 @@ int main()
 
 		// Handle keyboard input to move the shape
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
-			shape.move({ -1.f, 0.f });
+			shape.move({ -movementSpeed * deltaTime, 0.f });
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
-			shape.move({ 1.f, 0.f });
+			shape.move({ movementSpeed * deltaTime, 0.f });
 		}
 
 		window.clear();
